a8b77a694a6524e84a222df60147597612e34e35
Romario <romaolifirenko5@gmail.com>
2022-10-13 21:25:15 +0300

    Готовая ДЗ

---
+++ main.js
@@ -0,0 +1,88 @@
+// 1 ===============================================
+
+var myNum = 10;
+var myStr = "строка";
+var myBool = true;
+var myArr = [1, 2, 3, 4, 5];
+var myObj = { first: "First Name", last: "Last Name" };
+
+console.log(myNum);
+console.log(myStr);
+console.log(myBool);
+console.log(myArr);
+console.log(myObj);
+
+// 2==================================================
+
+var decimal2 = myNum.toFixed(2);
+console.log(decimal2);
+
+// 3=========================================================
+
+var i = 100;
+console.log(++i);
+console.log(--i);
+
+console.log(i++);
+console.log(i);
+
+console.log(i--);
+console.log(i);
+
+// 4==================================================
+
+var myTest = 20;
+
+myTest += myNum;
+console.log(myTest);
+
+myTest -= myNum;
+console.log(myTest);
+
+myTest *= myNum;
+console.log(myTest);
+
+myTest /= myNum;
+console.log(myTest);
+
+myTest %= 5;
+console.log(myTest);
+
+// 5========================================================
+
+var myPi = Math.PI;
+console.log(myPi);
+
+var myRound = Math.round(89.279);
+console.log(myRound);
+
+var myRandom = Math.random() * 10;
+console.log(myRandom);
+
+var myPow = Math.pow(3, 5);
+console.log(myPow);
+
+// 6========================================================
+
+var strObj = { str: "Мама мыла раму, рама мыла маму" };
+
+strObj.length = strObj.str.length;
+console.log(strObj);
+
+// 7============================================================
+
+var isRamaPos = strObj.str.indexOf("рама");
+console.log(isRamaPos);
+
+// 8=================================================================
+
+var strReplace = strObj.str.replace("мыла", "моет");
+
+strReplace = strReplace.replace("рама", "Рама");
+strReplace = strReplace.replace("мыла", "держит");
+console.log(strReplace);
+
+// 9 =================================================
+
+console.log(myStr.toUpperCase());
+console.log(myStr.toLowerCase());
